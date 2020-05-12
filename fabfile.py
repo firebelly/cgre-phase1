1a57779a2eb48e5e7890ce6a4f8917b48a4cff77
Nate Beaty <nate@clixel.com>
2020-05-12 11:51:38 -0400

    add form to dependencies file, update deploy script to install grav plugins

--- fabfile.py
+++ fabfile.py
@@ -5,6 +5,7 @@ env.hosts = ['cgre.firebelly.co']
 env.user = 'firebelly'
 env.path = '~/Firebelly/cgre-phase1'
 env.remotepath = '/home/firebelly/webapps/cgre_phase1'
+env.grav_theme = 'cgre'
 env.git_branch = 'master'
 env.warn_only = True
 
@@ -17,11 +18,19 @@ def deploy(composer='y'):
   update()
   if composer == 'y':
     composer_install()
+  clear_cache()
+
+def clear_cache():
+  with cd(env.remotepath):
+    run('bin/grav clearcache')
 
 def update():
   with cd(env.remotepath):
     run('git pull origin {0}'.format(env.git_branch))
+  # upload fonts not in repo
+  put('user/themes/{0}/fonts'.format(env.grav_theme), env.remotepath + '/user/themes/{0}/fonts/'.format(env.grav_theme))
 
 def composer_install():
   with cd(env.remotepath):
-    run('php74 ~/bin/composer.phar install')
+    run('~/bin/composer install')
+    run('bin/gpm update -y')
